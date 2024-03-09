118a2d602b5c253b844a33df12a84c9f35c36175
elnur-elnur <tahirzadee@gmail.com>
2024-03-10 02:15:52 +0400

    created mobile first sign-up ui

--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -5,7 +5,7 @@ export default function Input({ type, className = "", ...props }) {
   if (props.icon)
     return (
       <div className="flex border border-gray-300 rounded">
-        <input type={type} className={`${className} px-2 py-1`} {...props} />
+        <input type={type} className={`${className} px-2 py-1 bg-gray-50`} {...props} />
         <button className="inline-flex items-center justify-center px-3 text-gray-400">
           <AiOutlineSearch />
         </button>
@@ -15,7 +15,7 @@ export default function Input({ type, className = "", ...props }) {
     return (
       <input
         type={type}
-        className={`${className} border border-gray-300 rounded px-3 py-1`}
+        className={`${className} border border-gray-300 rounded px-3 py-1 bg-gray-50`}
         {...props}
       />
     );
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../components/ui/Input";
+import Button from "../components/ui/Button";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl flex-col mx-auto md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link
+            to="/"
+            className="self-center text-3xl sm:text-xl font-semibold dark:text-white"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 to-pink-500 rounded text-white">
+              V
+            </span>
+            Blog
+          </Link>
+          <p className="mt-5 text-sm">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium
+            corporis impedit aspernatur animi blanditiis libero labore saepe
+            nemo magnam dignissimos.
+          </p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-3">
+            <label htmlFor="username">
+              <p>Your username</p>
+              <Input
+                type="text"
+                placeholder="Username"
+                id="username"
+                className="w-full"
+              />
+            </label>
+            <label htmlFor="email">
+              <p>Your email</p>
+              <Input
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                className="w-full"
+              />
+            </label>
+            <label htmlFor="password">
+              <p>Your password</p>
+              <Input
+                type="password"
+                className="w-full"
+                placeholder="Password"
+                id="password"
+                autoComplete="on"
+              />
+            </label>
+
+            <div className="bg-gradient-to-r from-indigo-500 to-pink-500 p-1 rounded">
+              <Button
+                type="submit"
+                className="bg-white text-black py-2 px-4 rounded w-full hover:bg-transparent hover:text-white hover:border-white hover:transition-all hover:duration-300"
+              >
+                sign up
+              </Button>
+            </div>
+          </form>
+          <div className="flex items-center gap-2 text-sm mt-5">
+            <p>Have an account?</p>
+            <Link className="text-blue-500 underline" to="/sign-in">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
