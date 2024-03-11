2245e658ac8092a767cc31bcdd1fac2d5456babd
elnur-elnur <tahirzadee@gmail.com>
2024-03-11 19:44:48 +0400

    create signup api route / user api route test / installed bcryptjs

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 // install npm dotenv for environment variables
 dotenv.config();
@@ -12,6 +14,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Listening on port 3000 !!!!");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
